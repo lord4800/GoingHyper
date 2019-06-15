1cf839336609b410eded8b7bda5a0a19a242f28b
lord4800 <killerontheroad4800@gmail.com>
2019-06-16 00:38:56 +0300

    Midnight update, start work on first level

--- GoingHiper/Assets/Scripts/BallController.cs
+++ GoingHiper/Assets/Scripts/BallController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public enum VectorType
+    {
+        Left,
+        Right,
+        Forward
+    }
+
     [SerializeField] private float TURNOFF_Y = 0f;
     [SerializeField] private Vector3 moveVector;
     [SerializeField] private Animator animator;
     private bool move;
+    private Vector3 currentMoveVector;
 
     // Start is called before the first frame update
     void Start()
     {
         move = true;
+        Rotate(VectorType.Forward);
     }
 
     // Update is called once per frame
@@ -21,9 +30,16 @@ public class BallController : MonoBehaviour
         UpdatePosition();
     }
 
-    public void Rotate(bool isLeft)
+    public void Rotate(VectorType vectorType)
     {
         //RotateVector
+        Vector3 temp = moveVector;
+        switch (vectorType)
+        {
+            case VectorType.Forward: { currentMoveVector = new Vector3(0, 0, temp.z); break; }
+            case VectorType.Left: { currentMoveVector = new Vector3(-temp.z, 0, 0); break; }
+            case VectorType.Right: { currentMoveVector = new Vector3(temp.z, 0, 0); break; }
+        }
     }
 
     public void FallDownEvent()
@@ -33,15 +49,22 @@ public class BallController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        move = false;
-        animator.Play("ball_fall");
+        if (other.tag != "Rotate")
+        {
+            move = false;
+            animator.Play("ball_fall");
+        }
+        else
+        {
+            Rotate(other.GetComponent<Rotate>().VectorType);
+        }
     }
 
     private void UpdatePosition()
     {
         if (move)
         {
-            transform.position += moveVector * Time.deltaTime;
+            transform.position += currentMoveVector * Time.deltaTime;
         }
     }
 }
