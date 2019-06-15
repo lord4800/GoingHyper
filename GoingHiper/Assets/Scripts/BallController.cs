c7175cb9ac005d5966ccaa5be10c3b0622d13603
lord4800 <killerontheroad4800@gmail.com>
2019-06-15 14:58:15 +0300

    Prototype shit

---
+++ GoingHiper/Assets/Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private Vector2 moveVector;
+    public Action WinAction;
+
+
+    void Start()
+    {
+        RicidbodyCheck();
+    }
+
+    private void RicidbodyCheck()
+    {
+        if (rigidbody2D == null)
+            rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(false);
+        rigidbody2D.simulated = false;
+        if (WinAction != null)
+            WinAction();
+    }
+
+    void OnMouseDown()
+    {
+        rigidbody2D.AddForce(moveVector, ForceMode2D.Impulse);
+    }
+}
---
+++ GoingHiper/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private const string MOVE_TRIGGER = "move";
+    [SerializeField] Animator animator;
+
+    void Start()
+    {
+        AnimatorCheck();
+    }
+
+    private void AnimatorCheck()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        animator.SetTrigger(MOVE_TRIGGER);
+    }
+}
