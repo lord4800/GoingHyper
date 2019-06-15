8fbd7ed1a4eff5ab0f81869a13eab3300ec23789
lord4800 <killerontheroad4800@gmail.com>
2019-06-15 20:40:37 +0300

    [WIP] Balls

---
+++ GoingHiper/Assets/Scripts/BallController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Vector3 moveVector;
+    private bool move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    public void Rotate(bool isLeft)
+    {
+        //RotateVector
+    }
+
+    private void UpdatePosition()
+    {
+        if (move)
+        {
+            transform.position += moveVector * Time.deltaTime;
+        }
+    }
+}
