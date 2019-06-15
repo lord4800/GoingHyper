794e55d0edb43e29a95861e93f3a26ab3fe6d494
lord4800 <killerontheroad4800@gmail.com>
2019-06-15 18:20:47 +0300

    Update UI progress

--- GoingHiper/Assets/Scripts/ProgressController.cs
+++ GoingHiper/Assets/Scripts/ProgressController.cs
@@ -5,20 +5,7 @@ using UnityEngine.UI;
 
 public class ProgressController : MonoBehaviour
 {
-
     [SerializeField] Slider _slider;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void ChangeMaxValue(float max)
     {
